fc5fae74ad1d6ef553a8a48bf98a320a575f222b
Eli <me@eliellis.com>
2012-12-17 11:14:48 -0500

    Update fader.js

--- fader.js
+++ fader.js
@@ -17,65 +17,32 @@ if (window.Fader == null) {
 
 (function(w, d){
 
-    Fader.fadeOutWithId = function(id, time) {
-      this.fadeWithId(id, time, 100, 0);
-    };
-    
-    Fader.fadeInWithId = function(id, time) {
-      this.fadeWithId(id, time, 0, 100);
-    };
-    
-    Fader.fadeOutWithClass = function(className, index, time) {
-      this.fadeWithClass(className, index, time, 100, 0);
-    };
-    
-    Fader.fadeInWithClass = function(className, index, time) {
-      this.fadeWithClass(className, index, time, 0, 100);
-    };
-    
-    Fader.fadeOutElement = function(elem, time) {
-      this.fadeElement(elem, time, 100, 0);
-    };
-    
-    Fader.fadeInElement = function(elem, time) {
-      this.fadeElement(elem, time, 0, 100);
-    };
-    
-    Fader.fadeOutWithSelector = function(selector, time) {
-      this.fadeCss(selector, time, 100, 0);
-    };
-    
-    Fader.fadeInWithSelector = function(selector, time) {
-      this.fadeCss(selector, time, 0, 100);
-    };
-    
     /*
     Private functions
     */
-    this = {};
     
-    this.fadeCss = function(selector, time, ini, fin) {
+    fadeCss = function(selector, time, ini, fin) {
       var target = document.querySelectorAll(selector);
       for (var i in target){ //handle all the elements
-        thisexecuteFade(i, time, ini, fin);
+        executeFade(i, time, ini, fin);
       }
     };    
     
-    this.fadeWithId = function(id, time, ini, fin) {
+    fadeWithId = function(id, time, ini, fin) {
       var target = document.getElementById(id);
-      this.executeFade(target, time, ini, fin);
+      executeFade(target, time, ini, fin);
     };
     
-    this.fadeWithClass = function(className, index, time, ini, fin) {
+    fadeWithClass = function(className, index, time, ini, fin) {
       var target = document.getElementsByClassName(className)[index];
-      this.executeFade(target, time, ini, fin);
+      executeFade(target, time, ini, fin);
     };
     
-    this.fadeElement = function(elem, time, ini, fin) {
-      this.executeFade(elem, time, ini, fin);
+    fadeElement = function(elem, time, ini, fin) {
+      executeFade(elem, time, ini, fin);
     };
     
-    this.executeFade = function(target, time, ini, fin) {
+    executeFade = function(target, time, ini, fin) {
       var alpha = ini;
       var inc;
       if (fin >= ini) {
@@ -89,15 +56,48 @@ if (window.Fader == null) {
           if ((inc > 0 && alpha >= fin) || (inc < 0 && alpha <= fin)) {
             clearInterval(i);
           }
-          this.setAlpha(target, alpha);
+          setAlpha(target, alpha);
           alpha += inc;
         }, timer
       );
     };
     
-    this.setAlpha = function(target, alpha) {
+    setAlpha = function(target, alpha) {
       target.style.filter = "alpha(opacity="+ alpha +")";
       target.style.opacity = alpha/100
     };
   
+  
+    Fader.fadeOutWithId = function(id, time) {
+      fadeWithId(id, time, 100, 0);
+    };
+    
+    Fader.fadeInWithId = function(id, time) {
+      fadeWithId(id, time, 0, 100);
+    };
+    
+    Fader.fadeOutWithClass = function(className, index, time) {
+      fadeWithClass(className, index, time, 100, 0);
+    };
+    
+    Fader.fadeInWithClass = function(className, index, time) {
+      fadeWithClass(className, index, time, 0, 100);
+    };
+    
+    Fader.fadeOutElement = function(elem, time) {
+      fadeElement(elem, time, 100, 0);
+    };
+    
+    Fader.fadeInElement = function(elem, time) {
+      fadeElement(elem, time, 0, 100);
+    };
+    
+    Fader.fadeOutWithSelector = function(selector, time) {
+      fadeCss(selector, time, 100, 0);
+    };
+    
+    Fader.fadeInWithSelector = function(selector, time) {
+      fadeCss(selector, time, 0, 100);
+    };
+    
 }(window, document));
